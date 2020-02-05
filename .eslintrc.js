10614b6ef7ca1af883d589e8c10df866c8fc7cae
Marx Roberto <marxmoita@gmail.com>
2020-02-04 21:03:26 -0300

    criando cadastro de usuários e utilizando o bcrypt para criar o hash do password

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    "class-methodes-use-this": "off", // regra padrão para que todo metodo use a palavra this
+    "class-methods-use-this": "off", // regra padrão para que todo metodo use a palavra this
     "no-param-reassign": "off", // permite que receba um parametro e altere o parametro, é preciso devido ao sequelize
     "camelcase": "off", // off para pertmitir que as variaveis não sejam sempre camelcase.. nossaVariavel
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }], // erro caso declare variavel e não utilize, variavel next podera ser declarada sem usar
